1168b1dc4839f98329ca36c9e0c76a9c7e048f99
Nnamdi <nwabuokeinnamdi@gmail.com>
2019-09-16 08:23:27 +0100

    Implement default bean-id

--- src/com/luv2code/springDemoAnnotations/AnnotationDemoApp.java
+++ src/com/luv2code/springDemoAnnotations/AnnotationDemoApp.java
@@ -8,7 +8,8 @@ public class AnnotationDemoApp {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanAnnotation-applicationContext.xml");
 
         //Get the bean from spring container
-        Coach theCoach = context.getBean("thatSillyCoach",Coach.class);
+//        Coach theCoach = context.getBean("thatSillyCoach",Coach.class);
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
 
         //Call a method on the bean
         System.out.println(theCoach.getDailyWorkout());
--- src/com/luv2code/springDemoAnnotations/TennisCoach.java
+++ src/com/luv2code/springDemoAnnotations/TennisCoach.java
@@ -2,7 +2,8 @@ package com.luv2code.springDemoAnnotations;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+//@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach{
     @Override
     public String getDailyWorkout() {
